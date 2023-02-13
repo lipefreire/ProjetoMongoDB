4f069c1c140adcebcce423e45d99569dfcce1d7e
Felipe Freire <felipef12123@gmail.com>
2023-02-13 16:13:58 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express();
-
 app.use(express.json());
 require("./models/Artigo");
-const mongoose = require('mongoose')
 const Artigo = mongoose.model('artigo')
 
 
+app.use((req, res, next) => {
+    console.log("Acessou o middleware!");
+    app.use(cors());
+    next();
+})
+
+// Consultar todos os ids cadastrados
 app.get("/", (req, res) => {
     Artigo.find({}).then((artigo) => {
         return res.json(artigo)
-    }).catch((error) => {
+    }).catch((err) => {
         return res.status(400).json({
             error: true,
             message: "Nenhum artigo encontrado."
@@ -18,6 +25,19 @@ app.get("/", (req, res) => {
     })
 });
 
+// Consultar a partir de um ID
+app.get("/artigo/:id", (req, res) => {
+    Artigo.findOne({_id: req.params.id}).then((artigo) => {
+        return res.json(artigo);
+    }).catch((err) => {
+        return res.status(400).json({
+            error: true,
+            message: "Nenhum artigo encontrado para esse ID."
+        })
+    })
+})
+
+// Adicionar um ID
 app.post("/artigo", (req, res) => {
     const artigo = Artigo.create(req.body, (err) => {
         if(err) return res.status(400).json({
@@ -27,6 +47,34 @@ app.post("/artigo", (req, res) => {
         return res.status(200).json({
             error: false,
             message: "Artigo cadastrado com sucesso!"
+        })
+    })
+})
+
+// Atualizar um artigo através do seu ID
+app.put("/artigo/:id", (req, res) => {
+    const artigo = Artigo.updateOne({_id: req.params.id}, req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro. Não foi possível atualizar o artigo."
+        })
+        return res.json({
+            error: false,
+            message: "Artigo editado com sucesso!"
+        })
+    })
+})
+
+// Deletar a partir de um ID
+app.delete("/artigo/:id", (req, res) => {
+    const artigo = Artigo.deleteOne({_id: req.params.id}, req.body, (err) => {
+        if(err) return res.status(400).json({
+            error: true,
+            message: "Erro. Não foi possível deletar o artigo."
+        })
+        return res.json({
+            error: false,
+            message: "Artigo deletado com sucesso!"
         })
     })
 })
